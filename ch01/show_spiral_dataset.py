5b24d5364f26c7a9ab47732547a38a594f69cb6f
YaGiNA <yahyu10101@gmail.com>
2018-07-26 14:23:59 +0900

    Make part of plotting data as graph

--- ch01/show_spiral_dataset.py
+++ ch01/show_spiral_dataset.py
@@ -7,3 +7,11 @@ import matplotlib.pyplot as plt
 x, t = spiral.load_data()
 print(x, x.shape)
 print(t, t.shape)
+
+# plotting data
+N = 100
+CLS_NUM = 3
+makers = ["o", "x", "^"]
+for i in range(CLS_NUM):
+    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, maker=makers[i])
+plt.show()
