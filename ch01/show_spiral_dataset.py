bf784d683064d923a0077536dda6015cbc0dfd00
YaGiNA <yutachihaya@gmail.com>
2018-07-26 16:25:28 +0900

    Fix a typo: marker

--- ch01/show_spiral_dataset.py
+++ ch01/show_spiral_dataset.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("..")   #In order to import from parent dir.
+sys.path.append("..")   # In order to import from parent dir.
 from dataset import spiral
 import matplotlib.pyplot as plt
 
@@ -11,7 +11,7 @@ print(t, t.shape)
 # plotting data
 N = 100
 CLS_NUM = 3
-makers = ["o", "x", "^"]
+markers = ["o", "x", "^"]
 for i in range(CLS_NUM):
-    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, maker=makers[i])
+    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
 plt.show()
