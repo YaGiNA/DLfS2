f4f2d52f384e73fe52e660a6d38137bc1ca0b39c
YaGiNA <yahyu10101@gmail.com>
2018-07-26 13:09:33 +0900

    Add Sygmoid class

--- common/layers.py
+++ common/layers.py
@@ -19,3 +19,18 @@ class MatMul:
         dW = np.dot(self.x.T, dout)
         self.grads[0][...] = dW
         return dx
+
+
+class Sygmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
