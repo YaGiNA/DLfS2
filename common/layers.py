711e64700b17a6e3af124960b3ceba15e435ca69
YaGiNA <yahyu10101@gmail.com>
2018-07-26 13:04:39 +0900

    Add MatMul class

---
+++ common/layers.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+
+    def forward(self, x):
+        W, = self.params
+        out = no.dot(x, W)
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
