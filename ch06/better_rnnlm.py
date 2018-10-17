38b041067d296a333e9143c2fff7afc85cb260e2
YaGiNA <yahyu10101@gmail.com>
2018-10-17 15:26:41 +0900

    Change import style of np

--- ch06/better_rnnlm.py
+++ ch06/better_rnnlm.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('..')
 from common.time_layers import *
-from common.np import np
+from common.np import *
 from common.base_model import BaseModel
 from typing import List
 
--- ch06/train_better_rnnlm.py
+++ ch06/train_better_rnnlm.py
@@ -40,7 +40,8 @@ trainer = RnnlmTrainer(model, optimizer)
 
 best_ppl = float('inf')
 for epoch in range(max_epoch):
-    trainer.fit(xs, ts, max_epoch=1, batch_size=batch_size, time_size=time_size, max_grad=max_grad)
+    trainer.fit(xs, ts, max_epoch=1, batch_size=batch_size,
+                time_size=time_size, max_grad=max_grad)
     model.reset_state()
     ppl = eval_perplexity(model, corpus_val)
     print('valid perplexity: ', ppl)
