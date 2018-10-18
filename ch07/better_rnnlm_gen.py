b4b34320f8310c53b30af0e589b6b52191e3c015
YaGiNA <yahyu10101@gmail.com>
2018-10-18 13:26:56 +0900

    Add script to generate sentence by BetterRnnlm

---
+++ ch07/better_rnnlm_gen.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('..')
+from common.np import np
+from common.functions import softmax
+from ch06.rnnlm import Rnnlm
+from ch06.better_rnnlm import BetterRnnlm
+
+
+class BetterRnnlmGen(BetterRnnlm):
+    def generate(self, start_id, skip_ids=None, sample_size=100):
+        word_ids = [start_id]
+        x = start_id
+        while len(word_ids) < sample_size:
+            x = np.array(x).reshape(1, 1)
+            score = self.predict(x)
+            p = softmax(score.flatten())
+
+            sampled = np.random.choice(len(p), size=1, p=p)
+            if (skip_ids is None) or (sampled not in skip_ids):
+                x = sampled
+                word_ids.append(int(x))
+
+        return word_ids
